b92ffcbb54b985de544967b8ac4cdf2b8994f362
alzoi <alzoi@ya.ru>
2020-01-21 17:54:24 +0500

    Update async.js

--- async.js
+++ async.js
@@ -1,45 +1,55 @@
 const fs = require('fs');
 const util = require('util');
 
-// // Читаем список файлов текущей директории (вариант с использованием колбэков).
-// fs.readdir(__dirname, (err, files)=>{
-//   if(err) throw err;
-//   // Идём по списку файлов.
-//   files.forEach(file => {
-//     fs.readFile(file, 'utf-8', (err, fileData)=>{
-//       if(!err) {
-//         console.log(file + ' ====================================================');
-//         console.log(fileData);  
-//       }
-//     });
-//   });
-// });
+function read_cb() {
+// Читаем список файлов текущей директории (вариант с использованием колбэков).
 
-// Функции fs.readdir() и fs.readFile(), использующие колбэки, заворачиваем в обёртку, которая позволит использовать промисы.
-const readdirAsync  = util.promisify(fs.readdir);
-const readFileAsync = util.promisify(fs.readFile);
-
-
-async function test(){
-// Асинхронная функция.
-    
-    console.log('1) Находим файлы в папке.');
-    // Ждём завершения работы асинхронного чтения файлов текущей директории.
-    let files = await readdirAsync(__dirname);
+  console.log('1) Находим файлы в папке.');
+  fs.readdir(__dirname, (err, files)=>{
     console.log('1) Done!');
-  
+    if(err) throw err;
+    
     console.log('2) Читаем содержимое файлов.');
-    for (const file of files) {
-      // try {
-      //   const dataFile = await readFileAsync(file, 'utf8');
-      // } catch (e) {
-
-      // }
-      const dataFile = await readFileAsync(file, 'utf8').catch(err => console.log('Ошибка чтения файла: ' + err.code) );
-      console.log(file + ' ====================================================');
-      console.log(dataFile);  
-    }
-    console.log('2) Done!');
+    // Идём по списку файлов.
+    files.forEach((file) => {
+      fs.readFile(file, 'utf-8', (err, fileData)=>{
+        if(!err) {
+          console.log(file + ' ====================================================');
+          console.log(fileData);
+        } else {
+          console.log('Ошибка чтения файла: ' + err.code);
+        }
+      });
+    });
+  });
 }
 
-test();
+async function read_prom() {
+// Асинхронная функция.
+
+  // Функции fs.readdir() и fs.readFile(), использующие колбэки, заворачиваем в обёртку, которая позволит использовать промисы.
+  const readdirAsync  = util.promisify(fs.readdir);
+  const readFileAsync = util.promisify(fs.readFile);
+    
+  console.log('1) Находим файлы в папке.');
+  // Ждём завершения работы асинхронного чтения файлов текущей директории.
+  let files = await readdirAsync(__dirname).catch(err => {});
+  console.log('1) Done!');
+
+  console.log('2) Читаем содержимое файлов.');
+  for (const file of files) {
+    // try {
+    //   const dataFile = await readFileAsync(file, 'utf8');
+    // } catch (e) {
+    // }
+
+    const dataFile = await readFileAsync(file, 'utf8').catch(err => console.log('Ошибка чтения файла: ' + err.code) );
+    console.log(file + ' ====================================================');
+    console.log(dataFile);  
+  }
+  console.log('2) Done!');
+
+}
+
+read_cb();
+//read_prom();
